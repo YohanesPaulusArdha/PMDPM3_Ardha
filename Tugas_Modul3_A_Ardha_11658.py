8fae76fd34e8f3a197dd7383f0356c315bc1ed27
YohanesPaulusArdha <220711658@students.uajy.ac.id>
2024-10-05 21:34:50 +0700

    Update Tugas_Modul3_A_Ardha_11658.py

--- Tugas_Modul3_A_Ardha_11658.py
+++ Tugas_Modul3_A_Ardha_11658.py
@@ -73,8 +73,8 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #0073e6;'>Data yang diupload:</h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'"D:\Semester 5\PMDPM\Pertemuan 3\Tugas3_A_11658"'
-    model_path = os.path.join(model_directory, r'GBR_IPK_model.pkl')
+    
+    model_path = r'GBR_IPK_model.pkl'
 
     if os.path.exists(model_path):
         with open(model_path, 'rb') as f:
